24865cea6d6859feb72884c84ec03c8586a59abf
Ruvindu <ruvindu4@gmail.com>
2026-07-12 12:06:58 +0000

    top level sorted

---
+++ SpaceWire/Top.vhd
@@ -0,0 +1,47 @@
+
+Library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+Entity:
+entity Top_Level is 
+port (
+    FirstPort : in std_logic;
+    SecondPort : out std_logic;
+    ThirdPort : inout std_logic_vector(15 downto 0)
+);
+end entity;
+
+-- Entity Declaration x 2
+    Entity Link_Management is
+        port(
+
+        );
+    end entity;
+
+    Entity Data_Packer is
+        port(
+
+        );
+    end entity;
+
+
+Architecure Instantiation of Top_Level is
+
+-- Component Declaration x 2
+    component Link_Management is
+        port(
+            -- port declaration here.
+        );
+    end component;
+    
+    component Data_Packer is
+        port(
+            -- port declaration here.
+        );
+    end component;
+
+
+
+
+End Architecure;
