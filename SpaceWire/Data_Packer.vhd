dc6f68b2c6a686056caf4f011c63a3065391f6be
Ruvindu <ruvindu4@gmail.com>
2026-07-15 23:27:11 +0000

    Data_packer started

--- SpaceWire/Data_Packer.vhd
+++ SpaceWire/Data_Packer.vhd
@@ -11,5 +11,22 @@ use ieee.numeric_std.all;
 
 Architecture RTL of Data_Packer is
 
+signal 3counter : unsigned(2 downto 0) := (others => '0');
+
+
+begin
+
+Counter : process (clk,rst)
+    if reset = '1' then
+        3counter <= '0';
+        if rising_edge(clk) then
+            if 3counter = '3' then 
+                3counter <= 0;
+            else
+                3counter <= 3counter + 1;
+            end if;
+        end if; 
+    end if;
+
 
 End architecture;
